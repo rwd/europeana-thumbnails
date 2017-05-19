c1b680808e751807b04e47c5fb95d901481953f5
Richard Doe <richard.doe@rwdit.net>
2017-05-19 15:45:51 +0100

    Optimization refactor

--- lib/europeana/thumbnails/app.rb
+++ lib/europeana/thumbnails/app.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'aws-sdk'
 require 'newrelic_rpm'
 require 'sinatra'
 require 'europeana/thumbnails'
@@ -8,46 +9,52 @@ module Europeana
     ##
     # Sinatra app to respond to thumbnail requests
     class App < Sinatra::Base
+      use Rack::Deflater
+
       set :public_folder, 'public'
 
-      def respond_with_redirects?
-        ENV.key?('RESPOND_WITH_REDIRECTS')
+      configure do
+        set :s3, Aws::S3::Resource.new
+        set :bucket, settings.s3.bucket(ENV['AMAZON_S3_BUCKET'])
+        set :respond_with_redirects, ENV.key?('RESPOND_WITH_REDIRECTS')
       end
 
-      get '/' do
-        uri = params['uri']
-        type = if params.key?('type') && %w(SOUND VIDEO TEXT 3D IMAGE).include?(params['type'].upcase)
+      TYPES = %w(SOUND VIDEO TEXT 3D IMAGE).freeze
+
+      def generic_path
+        type = if params.key?('type') && TYPES.include?(params['type'].upcase)
                  params['type'].downcase
                else
                  'image'
                end
+        "/images/item-#{type}-large.gif"
+      end
+
+      get '/' do
+        uri = params['uri']
         size = params['size'] || 'FULL_DOC'
 
-        resource_size = size =~ /w400/i ? 'LARGE' : 'MEDIUM'
+        resource_size = size == 'w400' ? 'LARGE' : 'MEDIUM'
         resource_path = Digest::MD5.hexdigest(uri) + '-' + resource_size
-        resource_url = ENV['AMAZON_S3_URL'] + resource_path
 
-        uri = URI.parse(resource_url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
+        s3_object = settings.bucket.object(resource_path)
 
-        response = respond_with_redirects? ? http.head(uri.path) : http.get(uri.path)
-
-        if (400..500).cover?(response.code.to_i)
-          if respond_with_redirects?
-            redirect to(generic_path), 302
-          else
-            generic_path = "/images/item-#{type}-large.gif"
+        if settings.respond_with_redirects
+          redirect to(s3_object.exists? ? s3_object.public_url : generic_path), 302
+        else
+          begin
+            s3_response = s3_object.get
+            headers({
+              'Content-Length' => s3_response.content_length,
+              'Content-Type' => 'image/jpeg',
+              'ETag' => s3_response.etag,
+              'Last-Modified' => s3_response.last_modified.httpdate
+            })
+            status 200
+            body s3_response.body
+          rescue Aws::S3::Errors::NoSuchKey
             generic_filepath = File.expand_path("../../../../public#{generic_path}", __FILE__)
             send_file generic_filepath, type: 'image/gif'
-          end
-        else
-          if respond_with_redirects?
-            redirect to(resource_url), 302
-          else
-            headers response.header.to_hash.merge('Content-Type' => 'image/jpeg')
-            status response.code
-            body response.body
           end
         end
       end
